0ab19dfea28f27dcd5ac1c83a7db590e75c4f165
meghana242005 <meghana242005@gmail.com>
2025-04-23 15:12:56 +0530

    save

---
+++ demo.java
@@ -0,0 +1,13 @@
+public class demo {
+    public static void main(String[] args) {
+       
+        int num1 = 10;
+        int num2 = 5;
+
+        
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+        int sub = num1 - num2;
+        System.out.println("sub: " + sub);
+    }
+}
